f11edf095d6a790220e97642e96f60ff2965372d
estrattonbailey <ericstrattonbailey@gmail.com>
2017-09-09 13:31:38 -0400

    Fix some small errors

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const colors = require('colors')
 program
   .version(pkg.version)
   .option('-w --watch', 'recursively watch src directory')
-  .option('-e --env <env>', 'specify an environment', 'development')
-  .option('-d, --deploy <env>', 'deploy a theme')
+  .option('-e --env [env]', 'specify an environment')
+  .option('-d, --deploy [env]', 'deploy a theme')
   .option('--debug', 'enable available debugging')
   .parse(process.argv)
 
@@ -20,14 +20,7 @@ if (program.debug) {
   process.env.DEBUG = '*'
 }
 
-/**
- * Default, false
- */
-process.env.SLATER_HMR = false
-
 const config = require('./lib/config.js')(program)
-const watch = require('./lib/watch.js')(program, config)
-const deploy = require('./lib/deploy.js')(program, config)
 
 /**
  * Clear terminal bc it's prettier
@@ -38,7 +31,7 @@ process.stdout.write('\x1B[2J\x1B[0f')
  * Actions
  */
 if (program.watch) {
-  watch()
+  require('./lib/watch.js')(program, config)
 } else if (program.deploy) {
-  deploy()
+  require('./lib/deploy.js')(program, config)
 }
