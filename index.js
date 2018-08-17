0545602a965f6958467488de3fcf9fa731aa43cf
estrattonbailey <ericstrattonbailey@gmail.com>
2018-08-17 18:21:22 -0400

    A little cleanup

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 #! /usr/bin/env node
 'use strict'
 
+/**
+ * Clear terminal bc it's prettier
+ */
+process.stdout.write('\x1B[2J\x1B[0f')
+
 const pkg = require('./package.json')
 const path = require('path')
 const fs = require('fs-extra')
@@ -12,8 +17,7 @@ const postcss = require('rollup-plugin-postcss')
 const chokidar = require('chokidar')
 const match = require('anymatch')
 const yaml = require('yaml').default
-
-const dir = (...args) => path.join(process.cwd(), ...args)
+const socket = require('./lib/socket.js')
 
 const {
   _: args,
@@ -28,42 +32,17 @@ const watch = args[0] === 'watch'
 const build = args[0] === 'build'
 const deploy = args[0] === 'deploy'
 
-let ignoredFiles = [
+const ignoredFiles = [
   'scripts/**',
   'styles/**',
   /DS_Store/
 ]
+
 try {
   ignoredFiles.concat(require('parse-gitignore')(fs.readFileSync(dir('.gitignore'))))
 } catch (e) {}
 
-const server = require('https').createServer({
-  key: fs.readFileSync(__dirname + '/lib/cert/server.key'),
-  cert: fs.readFileSync(__dirname + '/lib/cert/server.crt')
-}, (req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  })
-  res.write('slater running')
-  res.end()
-})
-
-const io = require('socket.io')(server, {
-  serveClient: false
-})
-
-// io.on('connection', () => {
-//   console.log('connected')
-// })
-
-server.listen(3001)
-
-/**
- * Clear terminal bc it's prettier
- */
-// process.stdout.write('\x1B[2J\x1B[0f')
-
-const themeConfig = yaml.parse(fs.readFileSync(dir('config.yml'), 'utf8'))[env]
+const themeConfig = yaml.parse(fs.readFileSync(dir('build/config.yml'), 'utf8'))[env]
 
 const theme = themekit({
   password: themeConfig.password,
@@ -71,6 +50,10 @@ const theme = themekit({
   theme_id: themeConfig.theme_id,
   ignore_files: themeConfig.ignore_files
 })
+
+function dir (...args) {
+  return path.join(process.cwd(), ...args)
+}
 
 const tasks = {
   watch () {
@@ -83,31 +66,35 @@ const tasks = {
     })
       .on('add', p => {
         if (match(ignoredFiles, p)) return
+
         const pathname = p.split('/src')[1]
         const dest = dir('/build', pathname)
-        fs.copy(p, dest).catch(e => {
-          console.log(c.magenta(`copying ${pathname} failed`), e || '')
-        })
+
+        fs.copy(p, dest)
+          .catch(e => {
+            console.log(c.magenta(`copying ${pathname} failed`), e || '')
+          })
       })
       .on('change', p => {
         if (match(ignoredFiles, p)) return
+
         const pathname = p.split('/src')[1]
         const dest = dir('/build', pathname)
-        fs.copy(p, dest).catch(e => {
-          console.log(c.magenta(`copying ${pathname} failed`), e || '')
-        })
+
+        fs.copy(p, dest)
+          .catch(e => {
+            console.log(c.magenta(`copying ${pathname} failed`), e || '')
+          })
       })
       .on('unlink', p => {
         if (match(ignoredFiles, p)) return
-        const pathname = p.split('/src')[1]
-        fs.remove(dir('/build', pathname)).catch(e => {
-          console.log(c.magenta(`removing ${pathname} failed`), e || '')
-        })
-      })
 
-    theme.upload('templates/index.liquid', dir('build/templates/index.liquid'))
-      .then(res => {
-        console.log(res)
+        const pathname = p.split('/src')[1]
+
+        fs.remove(dir('/build', pathname))
+          .catch(e => {
+            console.log(c.magenta(`removing ${pathname} failed`), e || '')
+          })
       })
 
     /**
@@ -123,7 +110,7 @@ const tasks = {
 
         theme.upload(pathname, p)
           .then(() => {
-            io.emit('refresh')
+            socket.emit('refresh')
           })
           .catch(e => {
             console.log(c.magenta(`uploading ${pathname} failed`), e || '')
@@ -134,7 +121,7 @@ const tasks = {
 
         theme.upload(pathname, p)
           .then(() => {
-            io.emit('refresh')
+            socket.emit('refresh')
           })
           .catch(e => {
             console.log(c.magenta(`updating ${pathname} failed`), e || '')
@@ -145,7 +132,7 @@ const tasks = {
 
         theme.upload(pathname, p)
           .then(() => {
-            io.emit('refresh')
+            socket.emit('refresh')
           })
           .catch(e => {
             console.log(c.magenta(`removing ${pathname} failed`), e || '')
@@ -158,7 +145,7 @@ const tasks = {
         return !/scripts|styles/.test(src)
       }
     }).then(() => {
-      console.log(c.green(`  Copied theme.`))
+      console.log(c.green(`    Copied theme.`))
     })
   },
   compile () {
@@ -210,8 +197,11 @@ const tasks = {
 }
 
 if (watch) {
-  tasks.watch()
-  tasks.compile()
+  tasks.copyTheme()
+    .then(() => {
+      tasks.watch()
+      tasks.compile()
+    })
 } else if (build) {
   tasks.copyTheme()
     .then(tasks.compile)
