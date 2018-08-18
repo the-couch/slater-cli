7f31dc905e921d19b5ec5e3c91f1248785ca9246
estrattonbailey <ericstrattonbailey@gmail.com>
2018-08-17 20:37:37 -0400

    Fix themekit, custom bundler

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const chokidar = require('chokidar')
 const match = require('anymatch')
 const yaml = require('yaml').default
 const socket = require('./lib/socket.js')
+const bundler = require('./lib/bundler.js')
+const { log } = require('./lib/util.js')
 
 const {
   _: args,
@@ -33,8 +35,8 @@ const build = args[0] === 'build'
 const deploy = args[0] === 'deploy'
 
 const ignoredFiles = [
-  'scripts/**',
-  'styles/**',
+  '**/scripts/**',
+  '**/styles/**',
   /DS_Store/
 ]
 
@@ -55,162 +57,139 @@ function dir (...args) {
   return path.join(process.cwd(), ...args)
 }
 
-const tasks = {
-  watch () {
-    /**
-     * From /src dir
-     */
-    chokidar.watch(dir('/src'), {
-      persistent: true,
-      ignoreInitial: true
-    })
-      .on('add', p => {
-        if (match(ignoredFiles, p)) return
+function compiler (opts = {}) {
+  return bundler({
+    input: dir('/src/scripts/index.js'),
+    output: dir('/build/assets/index.js'),
+    alias: {
+      scripts: dir('/src/scripts'),
+      styles: dir('/src/styles')
+    },
+    banner: `
+      (function (href) {
+        const socketio = document.createElement('script')
+        socketio.src = 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.slim.js'
+        socketio.onload = init
+        document.head.appendChild(socketio)
+        function init () {
+          var socket = io(href)
+          socket.on('refresh', () => window.location.reload())
+        }
+      })('https://localhost:3001');
+    `,
+    compress: opts.compress
+  })
+}
 
-        const pathname = p.split('/src')[1]
-        const dest = dir('/build', pathname)
+function copyTheme () {
+  return fs.copy(dir('src'), dir('build'), {
+    filter: (src, dest) => {
+      return !/scripts|styles/.test(src)
+    }
+  }).then(() => {
+    log(c.green('copied theme to build/'))
+  })
+}
 
-        fs.copy(p, dest)
-          .catch(e => {
-            console.log(c.magenta(`copying ${pathname} failed`), e || '')
-          })
-      })
-      .on('change', p => {
-        if (match(ignoredFiles, p)) return
+function watchFiles () {
+  /**
+   * From /src dir
+   */
+  chokidar.watch(dir('/src'), {
+    persistent: true,
+    ignoreInitial: true
+  })
+    .on('add', p => {
+      if (match(ignoredFiles, p)) return
 
-        const pathname = p.split('/src')[1]
-        const dest = dir('/build', pathname)
+      const pathname = p.split('/src')[1]
+      const dest = dir('/build', pathname)
 
-        fs.copy(p, dest)
-          .catch(e => {
-            console.log(c.magenta(`copying ${pathname} failed`), e || '')
-          })
-      })
-      .on('unlink', p => {
-        if (match(ignoredFiles, p)) return
-
-        const pathname = p.split('/src')[1]
-
-        fs.remove(dir('/build', pathname))
-          .catch(e => {
-            console.log(c.magenta(`removing ${pathname} failed`), e || '')
-          })
-      })
-
-    /**
-     * From /build dir
-     */
-    chokidar.watch(dir('/build'), {
-      ignore: /DS_Store/,
-      persistent: true,
-      ignoreInitial: true
-    })
-      .on('add', p => {
-        const pathname = p.split('/build')[1]
-
-        theme.upload(pathname, p)
-          .then(() => {
-            socket.emit('refresh')
-          })
-          .catch(e => {
-            console.log(c.magenta(`uploading ${pathname} failed`), e || '')
-          })
-      })
-      .on('change', p => {
-        const pathname = p.split('/build')[1]
-
-        theme.upload(pathname, p)
-          .then(() => {
-            socket.emit('refresh')
-          })
-          .catch(e => {
-            console.log(c.magenta(`updating ${pathname} failed`), e || '')
-          })
-      })
-      .on('unlink', p => {
-        const pathname = p.split('/build')[1]
-
-        theme.upload(pathname, p)
-          .then(() => {
-            socket.emit('refresh')
-          })
-          .catch(e => {
-            console.log(c.magenta(`removing ${pathname} failed`), e || '')
-          })
-      })
-  },
-  copyTheme () {
-    return fs.copy(dir('src'), dir('build'), {
-      filter: (src, dest) => {
-        return !/scripts|styles/.test(src)
-      }
-    }).then(() => {
-      console.log(c.green(`    Copied theme.`))
-    })
-  },
-  compile () {
-    return bili.write({
-      input: dir('/src/scripts/index.js'),
-      outDir: dir('/build/assets'),
-      filename: 'index.js',
-      format: [ watch ? 'iife' : 'iife-min' ],
-      target: 'browser',
-      watch: watch,
-      banner: watch ? `
-        (function (href) {
-          const socketio = document.createElement('script')
-          socketio.src = 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.slim.js'
-          socketio.onload = init
-          document.head.appendChild(socketio)
-
-          function init () {
-            var socket = io(href)
-            socket.on('refresh', () => window.location.reload())
-          }
-        })('https://localhost:3001');
-      ` : ``,
-      jsx: props.jsx || 'React.createElement',
-      plugins: [
-        require('rollup-plugin-alias')({
-          resolve: [ '.js', '.css' ],
-          scripts: dir('/src/scripts'),
-          styles: dir('/src/styles')
+      fs.copy(p, dest)
+        .catch(e => {
+          log(
+            c.red(`copying ${pathname} failed`),
+            e.message || e || ''
+          )
         })
-      ],
-      postcss: {
-        extract: !watch,
-        minimize: true,
-        plugins: [
-          require('postcss-import')(),
-          require('postcss-cssnext')({
-            warnForDuplicates: false
-          }),
-          require('postcss-nested'),
-          require('postcss-discard-comments')
-        ]
-      }
     })
-      .catch(e => {
-        console.error('compilation error', e)
-      })
-  }
+    .on('change', p => {
+      if (match(ignoredFiles, p)) return
+
+      const pathname = p.split('/src')[1]
+      const dest = dir('/build', pathname)
+
+      fs.copy(p, dest)
+        .catch(e => {
+          log(
+            c.red(`copying ${pathname} failed`),
+            e.message || e || ''
+          )
+        })
+    })
+    .on('unlink', p => {
+      if (match(ignoredFiles, p)) return
+
+      const pathname = p.split('/src')[1]
+
+      fs.remove(dir('/build', pathname))
+        .catch(e => {
+          log(
+            c.red(`removing ${pathname} failed`),
+            e.message || e || ''
+          )
+        })
+    })
+
+  /**
+   * From /build dir
+   */
+  chokidar.watch(dir('/build'), {
+    ignore: /DS_Store/,
+    persistent: true,
+    ignoreInitial: true
+  })
+    .on('add', p => {
+      const pathname = p.split('/build')[1]
+
+      theme.upload(pathname, p)
+        .then(() => socket.emit('refresh'))
+    })
+    .on('change', p => {
+      const pathname = p.split('/build')[1]
+
+      theme.upload(pathname, p)
+        .then(() => socket.emit('refresh'))
+    })
+    .on('unlink', p => {
+      const pathname = p.split('/build')[1]
+
+      theme.remove(pathname)
+        .then(() => socket.emit('refresh'))
+    })
 }
 
 if (watch) {
-  tasks.copyTheme()
-    .then(() => {
-      tasks.watch()
-      tasks.compile()
-    })
+  copyTheme().then(() => {
+    watchFiles()
+    compiler().watch()
+      .end(() => {
+        log(c.green(`compiled js/css`))
+      })
+  })
 } else if (build) {
-  tasks.copyTheme()
-    .then(tasks.compile)
-} else if (deploy) {
-  tasks.copyTheme()
-    .then(tasks.compile)
-    .then(() => theme('replace'))
-    .catch(e => {
-      console.error(e)
+  copyTheme().then(() => {
+    compiler({ compress: true }).compile().then(() => {
+      log(c.green(`compiled js/css`))
+      process.exit()
     })
+  })
+} else if (deploy) {
+  copyTheme().then(() => {
+    compiler({ compress: true }).compile().then(() => {
+      log(c.green(`compiled js/css`))
+      process.exit()
+    })
+  })
 }
 
